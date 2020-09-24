99271ea5de2d9fcd453451b75748425a61c101ca
yurenwang <sywyr2013@gmail.com>
2020-09-23 23:44:38 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {ColorPickerComponent} from './color-picker/color-picker.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ColorPickerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
